b22b80e134f76f76b27c26a5facfb6d4714f0d78
Prawin <pravinkumar.raja@skystack.io>
2024-12-16 09:01:49 +0530

    Updated Module folder only

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,14 +11,8 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
+
 
 rootProject.name = "ComposeAudioLib"
-include(":app")
+//include(":app")
 include(":composeAudioLib")
